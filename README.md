751e1fe1336b8afd5eafc5a8f08e990be212d172
S52000761 <52000761@student.tdtu.edu.vn>
2023-07-11 20:43:34 +0700

    upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # discordGPT-bot
+
+Change your API ChatGPT and Token discord bot in .evn file
+
+- run
+node index.js
---
+++ index.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
+
+client.on("messageCreate", async function (message) {
+    if (message.author.bot) return;
+
+    try {
+      const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [
+              {role: "system", content: "You are a helpful assistant who responds succinctly"},
+              {role: "user", content: message.content}
+          ],
+        });
+
+      const content = response.data.choices[0].message;
+      console.log(content);
+
+      // Chia chuỗi thành các phần nhỏ nếu vượt quá giới hạn 2000 từ của Discord
+      if (content.length > 2000) {
+        const chunks = content.match(/(.|[\r\n]){1,2000}/g);
+
+        for (const chunk of chunks) {
+          await message.channel.send(chunk);
+        }
+      } else {
+        await message.channel.send(content);
+      }
+      
+    } catch (err) {
+      console.error(err);
+      await message.reply(
+        "Something when wrong! Contact Hung for more infomation"
+      );
+    }
+  });
+
+  client.login(process.env.BOT_TOKEN);
